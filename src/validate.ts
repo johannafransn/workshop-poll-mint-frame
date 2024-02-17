d03f4b4aff4edcf00878da94dcab09b882686ccf
Johanna <adrina0002@gmail.com>
2024-02-17 15:57:29 +0100

    add new validation method

--- src/validate.ts
+++ src/validate.ts
@@ -1,16 +1,36 @@
-// Backend functions only
-import { Message, getSSLHubRpcClient } from '@farcaster/hub-nodejs'
-
-export async function validateMessage(messageBytes: string | undefined) {
+export async function validateMessage(
+  messageBytes: string | undefined
+): Promise<boolean> {
   if (!messageBytes) return false
 
-  const client = getSSLHubRpcClient('nemes.farcaster.xyz:2283')
-  const hubMessage = Message.decode(Buffer.from(messageBytes, 'hex'))
-  const res = await client.validateMessage(hubMessage)
+  const postData = {
+    cast_reaction_context: false,
+    follow_context: false,
+    message_bytes_in_hex: messageBytes,
+  }
 
-  if (res.isOk() && res.value.valid) {
-    return true
-  } else {
-    return false
+  const url = 'https://api.neynar.com/v2/farcaster/frame/validate'
+  const apiKey = 'NEYNAR_API_DOCS'
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        api_key: apiKey,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    })
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    const responseData = await response.json()
+    return responseData.valid
+  } catch (error) {
+    console.error('Error validating frame:', error)
+    throw error
   }
 }
