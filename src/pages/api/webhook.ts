6f2cb86028293cfbb7a469bbf376b8f160f0f4fb
Johanna <adrina0002@gmail.com>
2024-02-17 15:51:47 +0100

    first commit

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,78 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { buffer } from 'micro'
+import { signatureHelper } from '@kentico/kontent-webhook-helper'
+import { SERVER_URL, generateFarcasterFrame } from '@/utils'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== 'POST') {
+      res.status(405).json({ error: 'Method Not Allowed' })
+      return
+    }
+
+    const rawBody = (await buffer(req)).toString()
+    const data = JSON.parse(rawBody)
+
+    const signatureHeader = req.headers['Syndicate-Signature']?.toString() || ''
+    const pairs = signatureHeader.split(',')
+    const signatureData: Record<string, string> = {}
+    pairs.forEach((pair) => {
+      const [prefix, value] = pair.trim().split('=')
+      signatureData[prefix] = value
+    })
+    const signature = signatureData.s
+
+    console.log('data', data, 'signature', signature)
+
+    // Validate the signature
+    const isValidSignature = signatureHelper.isValidSignatureFromString(
+      rawBody,
+      process.env.WEBHOOK_SECRET || '',
+      signature
+    )
+
+    if (!isValidSignature) {
+      console.error('Invalid signature')
+      return res.status(401).end()
+    }
+
+    console.log('Valid signature. Processing webhook payload...')
+
+    // @dev WIP because server cannot modify client session
+    /**
+		let frame
+		const status = data.data.status
+		const isSuccess = data.data.status === 'SUBMITTED'
+		const isLoading = data.data.status === 'PENDING' || 'PROCESSED'
+
+		const currentPath = req.url || ''
+		const isOnSamePath = currentPath === ''
+
+
+		switch (status && !isOnSamePath) {
+		case isSuccess:
+			frame = generateFarcasterFrame(`${BASE_URL}/redirect.png`, 'redirect')
+			break
+		case isLoading:
+			frame = generateFarcasterFrame(`${BASE_URL}/loading.png`, 'error')
+			break
+		default:
+			frame = generateFarcasterFrame(`${BASE_URL}/error.png`, 'error')
+		}
+
+		return res.status(200).send(frame)
+	*/
+  } catch (error) {
+    console.error('Error processing webhook:', error)
+    res.status(500).end()
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
---
+++ src/utils.ts
@@ -0,0 +1,111 @@
+import { sql } from '@vercel/postgres'
+
+import { syndicate } from './config'
+import { TPostData, TUntrustedData } from './types'
+
+export const SERVER_URL = process.env.SERVER_URL
+
+// generate an html page with the relevant opengraph tags
+export function generateFarcasterFrame(image: string, postData: TPostData) {
+  let metaTags = ''
+
+  switch (postData) {
+    case 'mint':
+      metaTags += `
+		  <meta property="fc:frame:image" content="${image}" />
+		  <meta property="fc:frame:button:1" content="Mint Johannas NFT ✨" />`
+      break
+    case 'redirect':
+      metaTags += `
+		  <meta property="fc:frame:image" content="${image}" />
+		  <meta property="fc:frame:button:1" content="Github" />
+		  <meta property="fc:frame:button:1:action" content="post_redirect" />`
+      break
+    case 'error':
+      metaTags += `
+		<meta property="fc:frame:image" content="${image}" />
+		<meta property="fc:frame:button:1" content="Follow me for the next release" />
+		<meta property="fc:frame:button:1:action" content="post_redirect" />`
+      break
+  }
+
+  const postUrl = `${SERVER_URL}/api/post?data=${postData}`
+
+  return `
+	  <!DOCTYPE html>
+	  <html lang="en">
+	  <head>
+		<meta property="fc:frame" content="vNext" />
+		${metaTags}
+		<meta property="fc:frame:post_url" content="${postUrl}" />
+	  </head>
+	  <body>
+	  </body>
+	  </html>
+	`
+}
+
+export async function saveTextInput(ud: TUntrustedData) {
+  const isMintedAndRowCount =
+    await sql`SELECT COUNT(*) AS rowCount, bool_or(isMinted) AS isMinted
+	FROM "Feedback" 
+	WHERE Fid = ${ud.fid};`
+
+  // @dev limit token issuance up to is minted
+  if (isMintedAndRowCount.rows[0].isminted) {
+    console.log('Fid already minted:', ud.fid)
+    return generateFarcasterFrame(`${SERVER_URL}/error.png`, 'error')
+  } else {
+    await sql`INSERT INTO "Feedback" (Fid, Text, Isminted) VALUES (${ud.fid}, ${ud.inputText}, false);`
+    return generateFarcasterFrame(`${SERVER_URL}/mint.png`, 'mint')
+  }
+}
+
+export async function mintWithSyndicate(fid: number) {
+  /* const syndicateMintTx = await syndicate.transact.sendTransaction({
+    projectId: process.env.PROJECT_ID || '',
+    contractAddress: process.env.CONTRACT_ADDRESS || '',
+    chainId: Number(process.env.CHAIN_ID) || 84532,
+    functionSignature: 'mint(address to)',
+    args: {
+      to: await getAddrByFid(fid),
+    },
+  })
+
+  console.log('Syndicate Transaction ID: ', syndicateMintTx.transactionId)
+  await sql`UPDATE "Feedback" SET Isminted = true WHERE Fid = ${fid};` */
+
+  // @todo loading frame so that nft has time to mint
+  return generateFarcasterFrame(`${SERVER_URL}/redirect.png`, 'redirect')
+}
+
+async function getAddrByFid(fid: number): Promise<string | void> {
+  console.log('Extracting address for FID: ', fid)
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+  }
+
+  try {
+    // Searchcaster API
+    const resp = await fetch(
+      `https://searchcaster.xyz/api/profiles?fid=${fid}`,
+      options
+    )
+    if (!resp.ok) {
+      throw new Error('Network response was not ok')
+    }
+    const data = await resp.json()
+
+    // Extract connected address if available, otherwise use address from body
+    const connectedAddress = data[0]?.connectedAddress || data[0]?.body.address
+    console.log('Connected address:', connectedAddress)
+
+    return connectedAddress
+  } catch (error) {
+    return console.error('Error fetching profile data:', error)
+  }
+}
